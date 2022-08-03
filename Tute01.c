555d1b08e5125aca6d293b4c8219a82a2d1b1e98
IT21839160 <110514300+IT21839160@users.noreply.github.com>
2022-08-03 04:17:56 +0000

    Tute1

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,16 @@
 
 #include <stdio.h>
 
-int main() {
-  
+int main(void)
+{
+  int no1,no2,sum;
+  float avg;
+  printf("Input marks of two subjects");
+  scanf("%d%d",&no1,&no2);
+  sum=no1+no2;
+  avg=sum/2;
+  printf("Sum is:%d",sum);
+  printf("Average is:%f",avg);
   return 0;
 }
 
